36c94b2a60fb51593a452ac0c3643c8fad659193
miriam-sullivan <maljayd42@gmail.com>
2019-10-28 15:36:03 -0500

    got first part done

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
+
     "img-src": "img/logo.png"
   },
   "cta": {
@@ -40,3 +41,51 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//nav
+let nav = document.querySelectorAll('nav a');
+
+//cta
+let cta = document.querySelector('h1');
+
+let cta = document.querySelector('button');
+
+//main-content
+let cta = document.querySelector('features-h4');
+
+let cta = document.querySelector('features-content');
+
+let cta = document.querySelector('features-content');
+
+let cta = document.querySelector('about-h4');
+
+let cta = document.querySelector('about-content');
+
+let cta = document.querySelector('middle-img-src');
+
+let cta = document.querySelector('services-h4');
+
+let cta = document.querySelector('services-content');
+
+let cta = document.querySelector('product-h4');
+
+let cta = document.querySelector('product-content');
+
+let cta = document.querySelector('vision-h4');
+
+
+//contact
+let cta = document.querySelector('vision-content');
+
+let cta = document.querySelector('contact-h4');
+
+let cta = document.querySelector('address');
+
+let cta = document.querySelector('phone');
+
+let cta = document.querySelector('email');
+
+
+//footer
+let cta = document.querySelector('copyright');
